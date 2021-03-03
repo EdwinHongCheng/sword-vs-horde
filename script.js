18b6470ef001cf2571d680e5e15b65fe76e4b4ad
Edwin Cheng <edwincheng3@gmail.com>
2021-03-03 08:41:05 -0800

    base

--- script.js
+++ script.js
@@ -1,18 +1,105 @@
-// [WIP] vid: 4:19+
 const canvas = document.getElementById('canvas1');
-const ctx = canvas.getConetxt('2d');
-canvas.width = 640;
-canvas.height = 640;
+const ctx = canvas.getContext('2d');
+canvas.width = 800;
+canvas.height = 500;
 
 const keys = [];
 
 const player = {
-    x: 0,
-    y: 0,
-    width: 16,
-    height: 32,
+    // [NOTE] 1st 2 values below = starting position (ex. 200, 200)
+    x: 200,
+    y: 300,
+    width: 40,
+    height: 72,
     frameX: 0,
     frameY: 0,
     speed: 9,
     moving: false
 }
+
+const playerSprite = new Image();
+playerSprite.src = "./test/chewie.png";
+const background = new Image();
+// [NOTE] change back to background.jpg later
+background.src = "./test/background1.png";
+
+// s = source X, Y, width, height
+// d = destination X, Y, width, height
+function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
+    ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
+}
+
+// Note: uses "keys" array (on line 6)
+window.addEventListener("keydown", function(e) {
+    // [NOTE] "e.keycode" ("e.keys" DOES NOT WORK)
+    keys[e.keyCode] = true;
+    player.moving = true;
+});
+
+window.addEventListener("keyup", function(e) {
+    delete keys[e.keyCode];
+    player.moving = false;
+});
+
+function movePlayer() {
+    // [Up]
+    if (keys[38] && player.y > 0) {
+        player.y -= player.speed;
+        player.frameY = 3;
+        player.moving = true;
+    }
+    // [Left]
+    if (keys[37] && player.x > 0) {
+        player.x -= player.speed;
+        player.frameY = 1;
+        player.moving = true;
+    }
+    // Down
+    if (keys[40] && player.y < canvas.height - player.height) {
+        player.y += player.speed;
+        player.frameY = 0;
+        player.moving = true;
+    }
+    // Right
+    if (keys[39] && player.x < canvas.width - player.width) {
+        player.x += player.speed;
+        player.frameY = 2;
+        player.moving = true;
+    }
+}
+
+function handlePlayerFrame() {
+    if (player.frameX < 3 && player.moving) player.frameX++
+    else player.frameX = 0;
+}
+
+let fps, fpsInterval, startTime, now, then, elapsed;
+
+function startAnimating(fps) {
+    fpsInterval = 1000/fps;
+    then = Date.now();
+    startTime = then;
+    animate();
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    now = Date.now();
+    elapsed = now - then;
+    if (elapsed > fpsInterval) {
+        then = now - (elapsed % fpsInterval);
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+        drawSprite(
+            playerSprite, player.width * player.frameX, player.height * player.frameY, 
+            player.width, player.height,
+            player.x, player.y, player.width, player.height
+        );
+        movePlayer();
+        handlePlayerFrame();
+    }
+}
+
+// [NOTE] change argument - larger number = more FPS = faster
+startAnimating(30);
