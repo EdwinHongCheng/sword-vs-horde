31e9e6b9d426a678ad6c2f20dfd6a63c686b22fe
Edwin Cheng <edwincheng3@gmail.com>
2021-03-03 12:32:29 -0800

    working on hero sprite attacks, etc

--- script.js
+++ script.js
@@ -15,12 +15,15 @@ const player = {
     frameY: 0,
     speed: 9,
     moving: false,
-    // [TEST] facing direction - determines attack animation
-    facing: "down"
+    // [TEST] attacking - facing direction - determines attack animation
+    facing: "down",
+    attacking: false
 }
 
 const playerSprite = new Image();
+
 playerSprite.src = "./test/swordsman_moving.png";
+
 const background = new Image();
 // [NOTE] change back to background.jpg later
 background.src = "./test/background.jpg";
@@ -46,6 +49,8 @@ window.addEventListener("keyup", function(e) {
 function movePlayer() {
     // [Up] "W"
     if (keys[87] && player.y > 0) {
+        playerSprite.src = "./test/swordsman_moving.png";
+        player.width = 16;
         player.y -= player.speed;
         player.frameY = 2;
         player.moving = true;
@@ -54,6 +59,8 @@ function movePlayer() {
     }
     // [Left] "A"
     if (keys[65] && player.x > 0 + (player.width / 2)) {
+        playerSprite.src = "./test/swordsman_moving.png";
+        player.width = 16;
         player.x -= player.speed;
         player.frameY = 3;
         player.moving = true;
@@ -61,6 +68,8 @@ function movePlayer() {
     }
     // [Down] "S"
     if (keys[83] && player.y < canvas.height - player.height) {
+        playerSprite.src = "./test/swordsman_moving.png";
+        player.width = 16;
         player.y += player.speed;
         player.frameY = 0;
         player.moving = true;
@@ -68,13 +77,27 @@ function movePlayer() {
     }
     // [Right] "D"
     if (keys[68] && player.x < canvas.width - player.width) {
+        playerSprite.src = "./test/swordsman_moving.png";
+        player.width = 16;
         player.x += player.speed;
         player.frameY = 1;
         player.moving = true;
         player.facing = "right";
-
-    // [TEST] Attack Animation
-
+    }
+    // [TEST] Attack Animation - sorta works, cut spritesheet tho
+    if (keys[79]) {
+        player.attacking = true;
+        player.width = 32;
+        if (player.facing === "down") {
+            player.frameY = 0;
+        } else if (player.facing === "up") {
+            player.frameY = 1;
+        } else if (player.facing === "left") {
+            player.frameY = 3;
+        } else if (player.facing === "right") {
+            player.frameY = 2;
+        }
+        playerSprite.src = "./test/swordsman_attacking.png";
     }
 }
 
