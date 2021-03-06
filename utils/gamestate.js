985c6a5b27eb02b0d2797e0e432a948d69492cd9
Edwin Cheng <edwincheng3@gmail.com>
2021-03-06 01:48:28 -0800

    fix instruction line spacing

--- utils/gamestate.js
+++ utils/gamestate.js
@@ -20,22 +20,22 @@ window.addEventListener("keydown", function() {
         ctx.fillStyle = 'white';
 
         ctx.font = '30px Helvetica';
-        ctx.fillText('Instructions', 324, 40);
+        ctx.fillText('Instructions', 324, 50);
         ctx.font = '22px Helvetica';
         // [Blurb]
-        ctx.fillText('Protect the village from the evil slime horde!', 20, 90);
-        ctx.fillText('Vanquish them all with thy trusty flame sword!', 20, 120);
-        ctx.fillText('Do not attempt to run back home.', 20, 150);
-        ctx.fillText('The battle waits for you alone.', 20, 180);
+        ctx.fillText('Protect the village from the evil slime horde!', 20, 100);
+        ctx.fillText('Vanquish them all with thy trusty flame sword!', 20, 140);
+        ctx.fillText('Do not attempt to run back home.', 20, 180);
+        ctx.fillText('The battle waits for you alone.', 20, 220);
         // [Controls]
 
         ctx.font = '30px Helvetica';
-        ctx.fillText('Buttons', 348, 250);
+        ctx.fillText('Buttons', 348, 270);
 
         ctx.font = '22px Helvetica';
-        ctx.fillText('Up / Left / Down / Right :   W / A / S / D', 20, 300);
-        ctx.fillText('Flame Sword Strike :   K', 20, 330);
-        ctx.fillText('Teleport Dash :   L', 20, 360);
+        ctx.fillText('Up / Left / Down / Right :   W / A / S / D', 20, 330);
+        ctx.fillText('Flame Sword Strike :   K', 20, 370);
+        ctx.fillText('Teleport Dash :   L', 20, 410);
 
         // ["Press Any Key to Start" Line]
         ctx.font = '26px Helvetica';
