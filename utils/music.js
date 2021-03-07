097b496f6a82f7b18e159da4458f88bbab8a1b3b
Edwin Cheng <edwincheng3@gmail.com>
2021-03-06 17:41:43 -0800

    add more music functionalities WIP

--- utils/music.js
+++ utils/music.js
@@ -1,7 +1,30 @@
 let gameMusic = new Audio("./music/save-our-planet.mp3");
 
-// [WORKS] loop music
+// [WORKS] Loops Music
 gameMusic.addEventListener('ended', function() {
   this.currentTime = 0;
   this.play();
 }, false);
+
+
+// [WORKS] Pause/Play Music (current: Space Bar)
+let pauseMusic = false;
+
+window.addEventListener('keydown', function(e) {
+  if (e.keyCode === 32) {
+    pauseMusic = !pauseMusic;
+    if (pauseMusic) {
+    gameMusic.pause();
+    } else {
+      gameMusic.play();
+    }
+  }
+});
+
+// [WORKS] Restart Music - KEY = "Y"
+window.addEventListener('keydown', function(e) {
+  // [KEY] "Y" = 89
+  if (e.keyCode === 89) {
+    gameMusic.currentTime = 0;
+  }
+});
