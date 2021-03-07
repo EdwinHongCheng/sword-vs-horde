e3719b5b2cc6500ce86dc802b9edfe7c5f108256
Edwin Cheng <edwincheng3@gmail.com>
2021-03-07 01:25:37 -0800

    add mute music toggle function

--- utils/music.js
+++ utils/music.js
@@ -8,33 +8,14 @@ gameMusic.addEventListener('ended', function() {
 
 
 // [WORKS] Pause/Play Music
-let pauseMusic = false;
-
 window.addEventListener('keydown', function(e) {
   // "G" = keyCode 71
   if (e.keyCode === 71 && currentState === 2 && !gameOver && !pauseGame) {
-    pauseMusic = !pauseMusic;
-    if (pauseMusic) {
-    gameMusic.pause();
-    } else {
-      gameMusic.play();
-    }
+    gameMusic.muted = !gameMusic.muted;
   }
 });
 
-
-// [WIP] Goal: use Restart
-// - Game Over/Beat Game -> pause Music + reset current time
-// - Restart Level -> play music again
-
-// [WORKS] Restart Music - KEY = "Y"
-// window.addEventListener('keydown', function(e) {
-//   // [KEY] "Y" = 89
-//   if (e.keyCode === 89) {
-//     gameMusic.currentTime = 0;
-//   }
-// });
-
+// [WORKS] Restart Music
 function handleGameMusic() {
   if (gameOver || beatEntireGame) {
     gameMusic.pause();
